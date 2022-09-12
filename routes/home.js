16c7e2b91ac83360b04c90bae099c7e7e659ac9d
Jason Cassella <jasoncassella1@gmail.com>
2022-09-12 00:40:26 -0400

    i use semicolons dog

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const router = express.Router()
-const homeController = require('../controllers/home')
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/home');
 
-router.get('/', homeController.getIndex) 
+router.get('/', homeController.getIndex);
 
-module.exports = router
+module.exports = router;
